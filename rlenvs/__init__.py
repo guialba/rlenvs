018dfe9bc2418834799ae32390ffd0d84e6b70e4
Gui <guilherme.albarrans@gmail.com>
2024-09-14 15:13:06 -0300

    correcao impot modulos

--- rlenvs/__init__.py
+++ rlenvs/__init__.py
@@ -1,9 +1,5 @@
 __version__ = 0.1
 
-# from rlenvs import river
-# from rlenvs import hunting
-# from rlenvs import texas_holdem_simp
-
-# River = river.River
-# Hunting = hunting.Hunting
-# Texas_Holdem_Simp = texas_holdem_simp.Texas_Holdem_Simp
+from rlenvs.river import *
+from rlenvs.hunting import *
+from rlenvs.texas_holdem_simp import *
