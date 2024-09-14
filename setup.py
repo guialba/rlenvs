9f718350e0f663ab8bcdac1393c5b451f4be4a5c
Gui <guilherme.albarrans@gmail.com>
2024-09-14 16:03:51 -0300

    ...

--- setup.py
+++ setup.py
@@ -4,13 +4,10 @@ import rlenvs
 setup(
     name='rlenvs',
     version=rlenvs.__version__,
-
     url='https://github.com/guialba/rlenvs',
     author='Guiulherme Albarrans Leite',
-
     install_requires=[
         'numpy',
     ],
     packages=find_packages()
-    
 )
