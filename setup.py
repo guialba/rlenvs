a79240c6c141457d22a43dc64d56836a269ecbb1
Gui <guilherme.albarrans@gmail.com>
2024-09-14 16:07:29 -0300

    correcao version

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
-import rlenvs
+# import rlenvs
 
 setup(
     name='rlenvs',
-    version=rlenvs.__version__,
+    # version=rlenvs.__version__,
+    version=0.1,
     url='https://github.com/guialba/rlenvs',
     author='Guiulherme Albarrans Leite',
     install_requires=[
