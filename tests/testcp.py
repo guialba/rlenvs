7f9b799bd5d1b41eb86f2471c6e08862f88ead0f
Gui <guilherme.albarrans@gmail.com>
2024-09-30 09:21:01 -0300

    ajuste teste

--- tests/testcp.py
+++ tests/testcp.py
@@ -7,18 +7,27 @@ n=500
 env = gym.make("custom/CartPole-v1", render_mode="human")
 env.action_space.seed(82)
 
-observation, info = env.reset(seed=82, masspole=1.45, length=1.0)
-
+observation, info = env.reset(seed=82, masspole=.45, length=1.0)
+hist_s = [observation]
+hist_a = []
 for _ in range(n):
     # action = env.action_space.sample()
     # action = 1
     action = int(input())
-    print('action: ', action)
+    if not(action in [0,1]):
+        break
+    # print('action: ', action)
     observation,reward, terminated, truncated, info = env.step(action)
-    print("info : ",info);
+    # print("observation : ",observation);
     
+    hist_s.append(observation)
+    hist_a.append(action)
+
+
     if terminated or truncated:
         break
         # observation, info = env.reset()
         
 env.close()
+
+print([{'s': s, "a": a} for s,a in zip(hist_s, hist_a)])
