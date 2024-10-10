34dc757b07ea87c04b0bfd3cb205a9d4f1a17bfe
Gui <guilherme.albarrans@gmail.com>
2024-10-10 11:34:50 -0300

    Add Discretized cart pole env

--- rlenvs/__init__.py
+++ rlenvs/__init__.py
@@ -1,4 +1,4 @@
-__version__ = 0.2
+__version__ = 0.25
 
 from rlenvs.river import *
 from rlenvs.hunting import *
@@ -11,6 +11,22 @@ register(
     id='custom/CartPole-v1',
     entry_point='rlenvs.cart_pole_custom:CustomCartPoleEnv',
     vector_entry_point="rlenvs.cart_pole_custom:CustomCartPoleVectorEnv",
+    max_episode_steps=500,
+    reward_threshold=475.0,
+)
+
+register(
+    id='custom/DiscreteCartPole-v1',
+    entry_point='rlenvs.cart_pole_custom:DiscreteCartPoleEnv',
+    vector_entry_point="rlenvs.cart_pole_custom:DiscreteCartPoleEnvVectorEnv",
+    max_episode_steps=500,
+    reward_threshold=475.0,
+)
+
+register(
+    id='custom/DynamicDiscreteCartPole-v1',
+    entry_point='rlenvs.cart_pole_custom:CustomDiscreteCartPoleEnv',
+    vector_entry_point="rlenvs.cart_pole_custom:CustomDiscreteCartPoleVectorEnv",
     max_episode_steps=500,
     reward_threshold=475.0,
 )
